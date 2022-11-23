301c12f8f780891514842ba85c6fe1fe13477117
Maria Adelaida Patiño Ocampo <maria-563@hotmail.com>
2022-11-23 14:07:59 -0500

    configurando base de datos en la nube

--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
-const host= "localhost";
-const port="27017";
+// const host= "localhost";
+// const port="27017";
+// const db ="hr";
+const host= "admin";
+const port="@micluster.orbni5f.mongodb.net";
 const db ="hr";
-
+//mongodb+srv://admin:admin@micluster.orbni5f.mongodb.net/hrretryWrites=true&w=majority
 exports.mongoConnect=()=>{
-    const mongoStringConnetion =`mongodb://${host}:${port}/${db}`;
+    //const mongoStringConnetion =`mongodb://${host}:${port}/${db}`;
+
+    const mongoStringConnetion =`mongodb+srv://admin${host}:${port}/${db}?retryWrites=true&w=majority`;
 
     mongoose.connect(mongoStringConnetion);
     mongoose.Promise= global.Promise;
