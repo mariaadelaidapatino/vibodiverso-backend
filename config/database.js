5d9e2863dd45b57368a9ee64411682c64dd036fc
Maria Adelaida Patiño Ocampo <maria-563@hotmail.com>
2022-11-25 09:20:41 -0500

    25/11/22

--- config/database.js
+++ config/database.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 // const host= "localhost";
 // const port="27017";
 // const db ="hr";
-const host= "admin";
-const port="@micluster.orbni5f.mongodb.net";
-const db ="hr";
+// const host= "admin";
+// const port="@micluster.orbni5f.mongodb.net";
+// const db ="hr";
 //mongodb+srv://admin:admin@micluster.orbni5f.mongodb.net/hr?retryWrites=true&w=majority
 exports.mongoConnect=()=>{
     //const mongoStringConnetion =`mongodb://${host}:${port}/${db}`;
 
-    const mongoStringConnetion =`mongodb+srv://admin${host}:${port}/${db}?retryWrites=true&w=majority`;
+    // const mongoStringConnetion =`mongodb+srv://admin${host}:${port}/${db}?retryWrites=true&w=majority`;
+
+    const mongoStringConnetion =`mongodb+srv://admin:admin@micluster.orbni5f.mongodb.net/hr?retryWrites=true&w=majority`;
 
     mongoose.connect(mongoStringConnetion);
     mongoose.Promise= global.Promise;
